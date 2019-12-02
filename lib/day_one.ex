7a3e424290c7c977cb1e01fcd38f1ff5e7c7b1b5
Manzanit0 <unico.javier@gmail.com>
2019-12-02 22:15:42 +0000

    Clean up day 1 solutions

--- lib/day_one.ex
+++ lib/day_one.ex
@@ -2,19 +2,15 @@ defmodule DayOne do
   def solution() do
     part_1 =
       "./priv/day_one_1.txt"
-      |> Path.expand()
-      |> File.stream!()
-      |> Stream.map(&sanitise/1)
+      |> stream_file_lines()
       |> Stream.map(&calculate_fuel/1)
-      |> Enum.reduce(0, &Kernel.+/2)
+      |> aggregate_fuel_results()
 
     part_2 =
       "./priv/day_one_2.txt"
-      |> Path.expand()
-      |> File.stream!()
-      |> Stream.map(&sanitise/1)
+      |> stream_file_lines()
       |> Stream.map(&calculate_fuel_recursively/1)
-      |> Enum.reduce(0, &Kernel.+/2)
+      |> aggregate_fuel_results()
 
     %{solution_1: part_1, solution_2: part_2}
   end
@@ -36,8 +32,17 @@ defmodule DayOne do
     calculate_fuel_recursively(fuel_required, accumuluated_fuel + fuel_required)
   end
 
+  defp aggregate_fuel_results(fuels), do: Enum.reduce(fuels, 0, &Kernel.+/2)
+
   defp positivise(num) when num < 0, do: 0
   defp positivise(num), do: num
+
+  defp stream_file_lines(file_name) do
+    file_name
+    |> Path.expand()
+    |> File.stream!()
+    |> Stream.map(&sanitise/1)
+  end
 
   defp sanitise(row) do
     row
