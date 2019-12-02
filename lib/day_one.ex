06819d8b31bdbef4bab2bc3deaa1fa079b9d529c
Manzanit0 <unico.javier@gmail.com>
2019-12-02 22:07:43 +0000

    Day one, part two

--- lib/day_one.ex
+++ lib/day_one.ex
@@ -1,11 +1,22 @@
 defmodule DayOne do
   def solution() do
-    "./priv/day_one.txt"
-    |> Path.expand()
-    |> File.stream!()
-    |> Stream.map(&sanitise/1)
-    |> Stream.map(&calculate_fuel/1)
-    |> Enum.reduce(0, &Kernel.+/2)
+    part_1 =
+      "./priv/day_one_1.txt"
+      |> Path.expand()
+      |> File.stream!()
+      |> Stream.map(&sanitise/1)
+      |> Stream.map(&calculate_fuel/1)
+      |> Enum.reduce(0, &Kernel.+/2)
+
+    part_2 =
+      "./priv/day_one_2.txt"
+      |> Path.expand()
+      |> File.stream!()
+      |> Stream.map(&sanitise/1)
+      |> Stream.map(&calculate_fuel_recursively/1)
+      |> Enum.reduce(0, &Kernel.+/2)
+
+    %{solution_1: part_1, solution_2: part_2}
   end
 
   def calculate_fuel(mass) do
@@ -13,7 +24,20 @@ defmodule DayOne do
     |> Kernel./(3)
     |> Float.floor()
     |> Kernel.-(2)
+    |> positivise()
   end
+
+  def calculate_fuel_recursively(mass, acc \\ 0)
+
+  def calculate_fuel_recursively(mass, acc) when mass <= 0, do: acc
+
+  def calculate_fuel_recursively(mass, accumuluated_fuel) do
+    fuel_required = calculate_fuel(mass)
+    calculate_fuel_recursively(fuel_required, accumuluated_fuel + fuel_required)
+  end
+
+  defp positivise(num) when num < 0, do: 0
+  defp positivise(num), do: num
 
   defp sanitise(row) do
     row
--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -10,4 +10,14 @@ defmodule DayOneTest do
       end
     end
   end
+
+  describe "part 2" do
+    test "recursive calculation of fuel" do
+      dataset = [{14, 2}, {1969, 966}, {100_756, 50346}]
+
+      for {mass, required_fuel} <- dataset do
+        assert required_fuel == DayOne.calculate_fuel_recursively(mass)
+      end
+    end
+  end
 end
